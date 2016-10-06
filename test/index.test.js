ab6f35eb6a824bf96337b6e130e9468867d4a2a1
Fredrik Liljegren <fredrik.liljegren@auth0.com>
2016-10-06 13:57:22 +0200

    Switching from jshint to eslint and making 'npm test' positive.

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,29 @@
+/* eslint-env mocha */
+/* global expect */
+
 var openid = require('index');
 
 
 describe('passport-openid', function() {
-    
+
   it('should export version', function() {
     expect(openid.version).to.be.a('string');
   });
-    
+
   it('should export Strategy', function() {
     expect(openid.Strategy).to.be.a('function');
   });
-  
+
   it('should export configuration functions', function() {
     expect(openid.disco).to.be.a('function');
     expect(openid.config).to.be.a('function');
     expect(openid.register).to.be.a('function');
   });
-  
+
   it('should export discovery mechanisms', function() {
     expect(openid.discovery).to.be.an('object');
     expect(openid.discovery.webfinger).to.be.a('function');
     expect(openid.discovery.lrdd).to.be.a('function');
   });
-  
+
 });
