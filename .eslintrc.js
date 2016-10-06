1c5241b32b71482d9fc9657b1d83fb9829dad88f
Fredrik Liljegren <fredrik.liljegren@auth0.com>
2016-10-06 14:04:32 +0200

    Removing some style overrides to get closer to auth0 js style.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,36 +9,29 @@ module.exports = {
   },
   rules: {
     'no-unused-vars': ['error', {args: 'none'}],
+    'object-shorthand': ['error', 'never'],
 
     // Rules set to warning because they are not followed but should be in the future.
     'max-len': 'warn',
+    'no-caller': 'warn',
     'no-mixed-operators': 'warn',
     'quote-props': 'warn',
 
     // Rules turned off because they are not followed.
     'block-scoped-var': 'off',
     'consistent-return': 'off',
-    'dot-notation': 'off',
     'eqeqeq': 'off',
     'guard-for-in': 'off',
-    'keyword-spacing': 'off',
     'no-else-return': 'off',
     'no-param-reassign': 'off',
     'no-restricted-syntax': 'off',
     'no-shadow': 'off',
     'no-underscore-dangle': 'off',
     'no-var': 'off',
-    'object-shorthand': 'off',
     'one-var': 'off',
-    'padded-blocks': 'off',
     'prefer-arrow-callback': 'off',
     'prefer-template': 'off',
-    'space-before-blocks': 'off',
-    'space-before-function-paren': 'off',
-    'spaced-comment': 'off',
     'vars-on-top': 'off',
-    'no-caller': 'off',
-    'no-proto': 'off',
-    'brace-style': 'off',
+    'no-proto': 'off'
   }
 };
