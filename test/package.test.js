9b8eb2314a2d85d3eda807b814267c35df23f592
Jared Hanson <jaredhanson@gmail.com>
2016-10-06 08:48:42 -0700

    Fix package tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,26 @@
-/* global describe, it */
-
-var pkg = require('..');
-var expect = require('chai').expect;
+var openid = require('..');
 
 
 describe('passport-openidconnect', function() {
+    
+  it('should export version', function() {
+    expect(openid.version).to.be.a('string');
+  });
+    
+  it('should export Strategy', function() {
+    expect(openid.Strategy).to.be.a('function');
+  });
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export configuration functions', function() {
+    expect(openid.disco).to.be.a('function');
+    expect(openid.config).to.be.a('function');
+    expect(openid.register).to.be.a('function');
+  });
+  
+  it('should export discovery mechanisms', function() {
+    expect(openid.discovery).to.be.an('object');
+    expect(openid.discovery.webfinger).to.be.a('function');
+    expect(openid.discovery.lrdd).to.be.a('function');
   });
   
 });
