b90783d8e7b003fc6f9e598605d41d6e0631c686
Jared Hanson <jaredhanson@gmail.com>
2016-10-06 10:16:30 -0700

    Clarify test cases.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -4,36 +4,40 @@ var Strategy = require('../lib/strategy')
 
 describe('Strategy', function() {
   
-  describe('issuing authorization request', function() {
+  describe('configured to work with a known OpenID provider', function() {
   
-    describe('that redirects to service provider without redirect URI', function() {
-      var strategy = new Strategy({
-        authorizationURL: 'https://www.example.com/oauth2/authorize',
-        tokenURL: 'https://www.example.com/oauth2/token',
-        clientID: 'ABC123',
-        clientSecret: 'secret'
-      },
-      function(accessToken, refreshToken, profile, done) {});
+    describe('issuing authorization request', function() {
+  
+      describe('that redirects to service provider without redirect URI', function() {
+        var strategy = new Strategy({
+          authorizationURL: 'https://www.example.com/oauth2/authorize',
+          tokenURL: 'https://www.example.com/oauth2/token',
+          clientID: 'ABC123',
+          clientSecret: 'secret'
+        },
+        function(accessToken, refreshToken, profile, done) {});
       
       
-      var url;
+        var url;
   
-      before(function(done) {
-        chai.passport.use(strategy)
-          .redirect(function(u) {
-            url = u;
-            done();
-          })
-          .req(function(req) {
-          })
-          .authenticate();
-      });
+        before(function(done) {
+          chai.passport.use(strategy)
+            .redirect(function(u) {
+              url = u;
+              done();
+            })
+            .req(function(req) {
+            })
+            .authenticate();
+        });
   
-      it('should be redirected', function() {
-        expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&client_id=ABC123&scope=openid');
-      });
-    }); // that redirects to service provider without redirect URI
+        it('should be redirected', function() {
+          expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&client_id=ABC123&scope=openid');
+        });
+      }); // that redirects to service provider without redirect URI
   
-  });
+    }); // issuing authorization request
+    
+  }); // configured to work with a known OpenID provider
   
 });
