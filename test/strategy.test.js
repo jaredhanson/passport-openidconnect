79fbc7ae5cec67829f6b4f762bda1e5da0432e2b
Jared Hanson <jaredhanson@gmail.com>
2016-10-07 07:25:17 -0700

    Remove scope separator option.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -164,6 +164,45 @@ describe('strategy', function() {
           expect(request.session['openidconnect:www.example.com'].state).to.equal(state);
         });
       }); // that redirects to identity provider with scope option
+      
+      describe('that redirects to identity provider with scope option as array', function() {
+        var strategy = new Strategy({
+          issuer: 'https://www.example.com',
+          authorizationURL: 'https://www.example.com/oauth2/authorize',
+          tokenURL: 'https://www.example.com/oauth2/token',
+          clientID: 'ABC123',
+          clientSecret: 'secret',
+          callbackURL: 'https://www.example.net/login/return'
+        }, function() {});
+      
+      
+        var request, url, state;
+  
+        before(function(done) {
+          chai.passport.use(strategy)
+            .redirect(function(u) {
+              var pu = uri.parse(u, true);
+              
+              state = pu.query.state;
+              url = u;
+              done();
+            })
+            .req(function(req) {
+              request = req;
+              req.session = {};
+            })
+            .authenticate({ scope: [ 'address', 'phone' ] });
+        });
+  
+        it('should be redirected', function() {
+          expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&client_id=ABC123&redirect_uri=https%3A%2F%2Fwww.example.net%2Flogin%2Freturn&scope=openid%20address%20phone&state=' + encodeURIComponent(state));
+        });
+        
+        it('should save state in session', function() {
+          expect(request.session['openidconnect:www.example.com'].state).to.have.length(24);
+          expect(request.session['openidconnect:www.example.com'].state).to.equal(state);
+        });
+      }); // that redirects to identity provider with scope option as array
   
     }); // issuing authorization request
     
