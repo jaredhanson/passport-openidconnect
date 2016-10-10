7a7ece31361aed41af4dfd1501a3c0f34f654b1f
Fredrik Liljegren <fredrik.liljegren@auth0.com>
2016-10-10 11:53:15 +0200

    Adding saveClientCallback, some docs and tests of dynamic registration.

--- README.md
+++ README.md
@@ -9,6 +9,38 @@ easily and unobtrusively integrated into any application or framework that
 supports [Connect](http://www.senchalabs.org/connect/)-style middleware,
 including [Express](http://expressjs.com/).
 
+
+## Dynamic flow
+
+If you don't provide `authorizationURL` and `tokenURL` in the strategy options,
+it is assumed that you want the OpenID Dynamic flow with automatic
+[Discovery](https://openid.net/specs/openid-connect-discovery-1_0.html) and/or
+[Registration](https://openid.net/specs/openid-connect-registration-1_0.html).
+
+The default Registrar module will use the following options:
+
+
+* `clientMetadata` (REQUIRED) - Information about your client, as an object, with the keys
+  described in
+  [Client Metadata](https://openid.net/specs/openid-connect-registration-1_0.html#ClientMetadata).
+  Note that `redirect_uris` is REQUIRED.
+
+* `getClientCallback` (REQUIRED) - `function(issuer, cb)`: call the `cb` function with
+  `(err, client)`.  `issuer` is the OpenID Connect provider,
+  e.g. `https://example.com`.  If you don't have a client for that issuer, use
+  `cb(null, null)` to commence registration.  If you have on, return it in the
+  following form `cb(null, client)`, where client is an object with:
+
+  * `id` (REQUIRED)
+  * `accessToken` (OPTIONAL)
+  * `secret` (OPTIONAL, as given from provider)
+  * `secretExpiresAt` (OPTIONAL, as given from provider)
+
+* `saveClientCallback` (OPTIONAL) - `function(client, cb)`: Called when a new client is
+  registered, with the client object as above.  When done, pass the client on
+  to the callback: `cb(null, client)`.
+
+
 ## Credits
 
   - [Jared Hanson](http://github.com/jaredhanson)
--- test/registrar.test.js
+++ test/registrar.test.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
 var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+
+// Mocks, stubs etc.
+var requestPost = sinon.stub(require('request'), 'post');
 
 // Code under test.
-var Registrar = require('../lib/registrar');
+var Registrar = proxyquire('../lib/registrar', { request: { post: requestPost } });
 
 describe('OpenID Connect Dynamic Registration', function () {
   describe('instantiation', function () {
@@ -16,16 +20,157 @@ describe('OpenID Connect Dynamic Registration', function () {
     });
   });
 
-  describe.skip('resolve getClientCallback', function () {
-    it('should check required client information', function () {
+  describe('registration request', function () {
+    it('should POST the clientMetadata as JSON to registrationURL', function () {
+      requestPost.reset();
+
       var getClientCallback = sinon.stub();
       var callback = sinon.spy();
-      var registrar = new Registrar({ getClientCallback: getClientCallback });
 
-      registrar.resolve('myIssuer', callback);
-      getClientCallback.yield('not object');
+      var registrar = new Registrar({
+        getClientCallback: getClientCallback,
+        clientMetadata: { foo: 'bar' }
+      });
 
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+
+      getClientCallback.yield(null, null); // Needed to pass on to the registration.
+
+      assert.equal(requestPost.callCount, 1);
+      assert.equal(requestPost.args[0][0].uri, 'myRegUrl');
+      assert.deepEqual(requestPost.args[0][0].json, { foo: 'bar' });
+    });
+
+    it('should fail on bad status code', function () {
+      requestPost.reset();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({ getClientCallback: function (i, cb) { cb(null, null); } });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      requestPost.yield(null, { statusCode: 500 }, null);
+
+      assert.equal(callback.callCount, 1);
       assert(callback.args[0][0] instanceof Error);
+    });
+
+    it('should fail on missing client_id', function () {
+      requestPost.reset();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({ getClientCallback: function (i, cb) { cb(null, null); } });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      requestPost.yield(null, { statusCode: 201 }, {});
+
+      assert.equal(callback.callCount, 1);
+      assert(callback.args[0][0] instanceof Error);
+    });
+
+    it('should fail on client_secret without expiration', function () {
+      requestPost.reset();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({ getClientCallback: function (i, cb) { cb(null, null); } });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      requestPost.yield(null, { statusCode: 201 }, { client_id: 1, client_secret: 42 });
+
+      assert.equal(callback.callCount, 1);
+      assert(callback.args[0][0] instanceof Error);
+    });
+
+    it('should yield formatted client on success', function () {
+      requestPost.reset();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({ getClientCallback: function (i, cb) { cb(null, null); } });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      requestPost.yield(null, { statusCode: 201 }, { client_id: 1 });
+
+      assert.equal(callback.callCount, 1);
+      assert.strictEqual(callback.args[0][0], null);
+      assert.deepEqual(callback.args[0][1], { id: 1, _json: { client_id: 1 } });
+    });
+  });
+
+  describe('getClientCallback', function () {
+    it('should use client if given', function () {
+      var getClientCallback = sinon.stub();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({ getClientCallback: getClientCallback });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      getClientCallback.yield(null, { id: 'foo' });
+
+      assert.equal(callback.callCount, 1);
+      assert.strictEqual(callback.args[0][0], null);
+      assert.deepEqual(callback.args[0][1], { id: 'foo' });
+    });
+
+    it('should fail if getClientCallback errors', function () {
+      var getClientCallback = sinon.stub();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({ getClientCallback: getClientCallback });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      getClientCallback.yield('myError');
+
+      assert.equal(callback.callCount, 1);
+      assert.strictEqual(callback.args[0][0], 'myError');
+    });
+  });
+
+  describe('saveClientCallback', function () {
+    it('should save client after registration', function () {
+      requestPost.reset();
+
+      var saveClientCallback = sinon.stub();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({
+        getClientCallback: function (i, cb) { cb(null, null); },
+        saveClientCallback: saveClientCallback
+      });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      requestPost.yield(null, { statusCode: 201 }, { client_id: 1 });
+
+      assert.equal(saveClientCallback.callCount, 1);
+      assert.deepEqual(saveClientCallback.args[0][0], { id: 1, _json: { client_id: 1 } });
+    });
+
+    it('should fail if saveClientCallback errors', function () {
+      requestPost.reset();
+
+      var saveClientCallback = sinon.stub();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({
+        getClientCallback: function (i, cb) { cb(null, null); },
+        saveClientCallback: saveClientCallback
+      });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      requestPost.yield(null, { statusCode: 201 }, { client_id: 1 });
+      saveClientCallback.yield('myError');
+
+      assert.equal(callback.callCount, 1);
+      assert.equal(callback.args[0][0], 'myError');
+    });
+
+    it('should pass on saveClientCallback\'s client object if modified', function () {
+      requestPost.reset();
+
+      var saveClientCallback = sinon.stub();
+      var callback = sinon.spy();
+
+      var registrar = new Registrar({
+        getClientCallback: function (i, cb) { cb(null, null); },
+        saveClientCallback: saveClientCallback
+      });
+      registrar.resolve({ registrationURL: 'myRegUrl', issuer: 'myIssuer' }, callback);
+      requestPost.yield(null, { statusCode: 201 }, { client_id: 1 });
+      saveClientCallback.yield(null, { id: 'one' });
+
+      assert.equal(callback.callCount, 1);
+      assert.strictEqual(callback.args[0][0], null);
+      assert.deepEqual(callback.args[0][1], { id: 'one' });
     });
   });
 });
