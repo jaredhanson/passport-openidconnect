4b00d31eea38f5b98402cdacaca2ff4608bb18f6
Jared Hanson <jaredhanson@gmail.com>
2021-10-25 08:49:56 -0700

    Update tests.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -113,7 +113,7 @@ describe('strategy', function() {
           .authenticate();
       }); // that redirects to identity provider with redirect URI and scope
       
-      describe('that redirects to identity provider with scope option', function() {
+      it('that redirects to identity provider with scope option', function(done) {
         var strategy = new Strategy({
           issuer: 'https://www.example.com',
           authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -124,38 +124,28 @@ describe('strategy', function() {
         }, function() {});
       
       
-        var request, url, state;
-  
-        before(function(done) {
-          chai.passport.use(strategy)
-            .redirect(function(u) {
-              var pu = uri.parse(u, true);
-              
-              state = pu.query.state;
-              url = u;
-              done();
-            })
-            .request(function(req) {
-              request = req;
-              req.session = {};
-            })
-            .authenticate({ scope: 'email' });
-        });
-  
-        it('should be redirected', function() {
-          expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&client_id=ABC123&redirect_uri=https%3A%2F%2Fwww.example.net%2Flogin%2Freturn&scope=openid%20email&state=' + encodeURIComponent(state));
-        });
-        
-        it('should save state in session', function() {
-          expect(request.session['openidconnect:www.example.com'].state.handle).to.have.length(24);
-          expect(request.session['openidconnect:www.example.com'].state.handle).to.equal(state);
+        chai.passport.use(strategy)
+          .redirect(function(url) {
+            var pu = uri.parse(url, true);
+            
+            expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&client_id=ABC123&redirect_uri=https%3A%2F%2Fwww.example.net%2Flogin%2Freturn&scope=openid%20email&state=' + encodeURIComponent(pu.query.state));
+            
+            expect(this.session['openidconnect:www.example.com'].state.handle).to.have.length(24);
+            expect(this.session['openidconnect:www.example.com'].state.handle).to.equal(pu.query.state);
 
-          expect(request.session['openidconnect:www.example.com'].state.authorizationURL).to.equal('https://www.example.com/oauth2/authorize');
-          expect(request.session['openidconnect:www.example.com'].state.tokenURL).to.equal('https://www.example.com/oauth2/token');
-          expect(request.session['openidconnect:www.example.com'].state.clientID).to.equal('ABC123');
-          expect(request.session['openidconnect:www.example.com'].state.clientSecret).to.equal('secret');
-          expect(request.session['openidconnect:www.example.com'].state.params.response_type).to.equal('code');
-        });
+            expect(this.session['openidconnect:www.example.com'].state.authorizationURL).to.equal('https://www.example.com/oauth2/authorize');
+            expect(this.session['openidconnect:www.example.com'].state.tokenURL).to.equal('https://www.example.com/oauth2/token');
+            expect(this.session['openidconnect:www.example.com'].state.clientID).to.equal('ABC123');
+            expect(this.session['openidconnect:www.example.com'].state.clientSecret).to.equal('secret');
+            expect(this.session['openidconnect:www.example.com'].state.params.response_type).to.equal('code');
+            
+            done();
+          })
+          .request(function(req) {
+            req.session = {};
+          })
+          .error(done)
+          .authenticate({ scope: 'email' });
       }); // that redirects to identity provider with scope option
       
       describe('that redirects to identity provider with scope option as array', function() {
