a1ae0ed5d65a489237fa8e5739c5dcf225b11ee1
Jared Hanson <jaredhanson@gmail.com>
2021-11-15 08:34:13 -0800

    Add test.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -111,6 +111,37 @@ describe('Strategy', function() {
       .authenticate();
   }); // should redirect with relative redirect URI
   
+  it('should redirect with scope as array', function(done) {
+    var strategy = new Strategy({
+      issuer: 'https://server.example.com',
+      authorizationURL: 'https://server.example.com/authorize',
+      tokenURL: 'https://server.example.com/token',
+      clientID: 's6BhdRkqt3',
+      clientSecret: 'some_secret12345',
+      callbackURL: 'https://client.example.org/cb',
+      scope: [ 'profile', 'email' ]
+    }, function() {});
+  
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.session = {};
+      })
+      .redirect(function(url) {
+        var l = uri.parse(url, true);
+        var state = l.query.state;
+        
+        expect(url).to.equal('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fclient.example.org%2Fcb&scope=openid%20profile%20email&state=' + encodeURIComponent(state));
+        expect(state).to.have.length(24);
+        expect(this.session['openidconnect:server.example.com'].state).to.deep.equal({
+          handle: state,
+          issuer: 'https://server.example.com'
+        });
+        done();
+      })
+      .error(done)
+      .authenticate();
+  }); // should redirect with scope as array
+  
   it('should redirect with scope as string', function(done) {
     var strategy = new Strategy({
       issuer: 'https://server.example.com',
