9e77aec10422b361bae0043f439a6bd0e4b90113
Jared Hanson <jaredhanson@gmail.com>
2021-11-15 09:12:07 -0800

    Clarify error message.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -670,72 +670,6 @@ describe('Strategy', function() {
       .authenticate();
   }); // should forbid request when issuer claim does not match identifier of OpenID provider
   
-  it('should forbid request when audience claim is a single string that does not contain the client ID value', function(done) {
-    var strategy = new Strategy({
-      issuer: 'https://server.example.com',
-      authorizationURL: 'https://server.example.com/authorize',
-      tokenURL: 'https://server.example.com/token',
-      userInfoURL: 'https://server.example.com/userinfo',
-      clientID: 's6BhdRkqt3',
-      clientSecret: 'some_secret12345',
-      callbackURL: 'https://client.example.org/cb'
-    },
-    function(iss, sub, profile, accessToken, refreshToken, cb) {
-      expect(iss).to.equal('https://server.example.com');
-      expect(sub).to.equal('248289761001');
-      var _raw = profile._raw; delete profile._raw;
-      var _json = profile._json; delete profile._json;
-      expect(profile).to.deep.equal({
-        id: '248289761001',
-        username: 'j.doe',
-        displayName: 'Jane Doe',
-        name: { familyName: 'Doe', givenName: 'Jane', middleName: undefined },
-        emails: [ { value: 'janedoe@example.com' } ]
-      });
-      expect(accessToken).to.equal('SlAV32hkKG');
-      expect(refreshToken).to.equal('8xLOxBtZp8');
-      
-      return cb(null, { id: '248289761001' }, { message: 'Hello' });
-    });
-    
-    sinon.stub(strategy._oauth2, 'getOAuthAccessToken').yieldsAsync(null, 'SlAV32hkKG', '8xLOxBtZp8', {
-      token_type: 'Bearer',
-      expires_in: 3600,
-      id_token: buildIdToken({}, undefined, 'XXXXXXXX')
-    });
-    
-    sinon.stub(strategy._oauth2, '_request').yieldsAsync(null, JSON.stringify({
-      sub: '248289761001',
-      name: 'Jane Doe',
-      given_name: 'Jane',
-      family_name: 'Doe',
-      preferred_username: 'j.doe',
-      email: 'janedoe@example.com',
-      picture: 'http://example.com/janedoe/me.jpg'
-    }));
-    
-    chai.passport.use(strategy)
-      .request(function(req) {
-        req.query = {
-          code: 'SplxlOBeZQQYbYS6WxSbIA',
-          state: 'af0ifjsldkj'
-        };
-        req.session = {};
-        req.session['openidconnect:server.example.com'] = {
-          state: {
-            handle: 'af0ifjsldkj'
-          }
-        };
-      })
-      .fail(function(challenge, status) {
-        expect(challenge).to.deep.equal({ message: 'aud parameter does not include this client - is: XXXXXXXX | expected: s6BhdRkqt3' });
-        expect(status).to.equal(403);
-        done();
-      })
-      .error(done)
-      .authenticate();
-  }); // should forbid request when audience claim is a single string that does not contain the client ID value
-  
   it('should forbid request when audience claim does not contain the client ID value', function(done) {
     var strategy = new Strategy({
       issuer: 'https://server.example.com',
@@ -747,21 +681,7 @@ describe('Strategy', function() {
       callbackURL: 'https://client.example.org/cb'
     },
     function(iss, sub, profile, accessToken, refreshToken, cb) {
-      expect(iss).to.equal('https://server.example.com');
-      expect(sub).to.equal('248289761001');
-      var _raw = profile._raw; delete profile._raw;
-      var _json = profile._json; delete profile._json;
-      expect(profile).to.deep.equal({
-        id: '248289761001',
-        username: 'j.doe',
-        displayName: 'Jane Doe',
-        name: { familyName: 'Doe', givenName: 'Jane', middleName: undefined },
-        emails: [ { value: 'janedoe@example.com' } ]
-      });
-      expect(accessToken).to.equal('SlAV32hkKG');
-      expect(refreshToken).to.equal('8xLOxBtZp8');
-      
-      return cb(null, { id: '248289761001' }, { message: 'Hello' });
+      throw new Error('verify function should not be called');
     });
     
     sinon.stub(strategy._oauth2, 'getOAuthAccessToken').yieldsAsync(null, 'SlAV32hkKG', '8xLOxBtZp8', {
@@ -794,13 +714,65 @@ describe('Strategy', function() {
         };
       })
       .fail(function(challenge, status) {
-        expect(challenge).to.deep.equal({ message: 'aud parameter does not include this client - is: XXXXXXXX,YYYYYYYY | expected to include: s6BhdRkqt3' });
+        expect(challenge).to.deep.equal({ message: 'ID token not intended for this relying party.' });
         expect(status).to.equal(403);
         done();
       })
       .error(done)
       .authenticate();
   }); // should forbid request when audience claim does not contain the client ID value
+  
+  it('should forbid request when audience claim is a single string that does not contain the client ID value', function(done) {
+    var strategy = new Strategy({
+      issuer: 'https://server.example.com',
+      authorizationURL: 'https://server.example.com/authorize',
+      tokenURL: 'https://server.example.com/token',
+      userInfoURL: 'https://server.example.com/userinfo',
+      clientID: 's6BhdRkqt3',
+      clientSecret: 'some_secret12345',
+      callbackURL: 'https://client.example.org/cb'
+    },
+    function(iss, sub, profile, accessToken, refreshToken, cb) {
+      throw new Error('verify function should not be called');
+    });
+    
+    sinon.stub(strategy._oauth2, 'getOAuthAccessToken').yieldsAsync(null, 'SlAV32hkKG', '8xLOxBtZp8', {
+      token_type: 'Bearer',
+      expires_in: 3600,
+      id_token: buildIdToken({}, undefined, 'XXXXXXXX')
+    });
+    
+    sinon.stub(strategy._oauth2, '_request').yieldsAsync(null, JSON.stringify({
+      sub: '248289761001',
+      name: 'Jane Doe',
+      given_name: 'Jane',
+      family_name: 'Doe',
+      preferred_username: 'j.doe',
+      email: 'janedoe@example.com',
+      picture: 'http://example.com/janedoe/me.jpg'
+    }));
+    
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.query = {
+          code: 'SplxlOBeZQQYbYS6WxSbIA',
+          state: 'af0ifjsldkj'
+        };
+        req.session = {};
+        req.session['openidconnect:server.example.com'] = {
+          state: {
+            handle: 'af0ifjsldkj'
+          }
+        };
+      })
+      .fail(function(challenge, status) {
+        expect(challenge).to.deep.equal({ message: 'ID token not intended for this relying party.' });
+        expect(status).to.equal(403);
+        done();
+      })
+      .error(done)
+      .authenticate();
+  }); // should forbid request when audience claim is a single string that does not contain the client ID value
   
   it('should forbid request when audience claim contain the client ID value but authorized party claim is not present', function(done) {
     var strategy = new Strategy({
