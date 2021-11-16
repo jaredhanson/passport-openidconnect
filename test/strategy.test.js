af62630171fcda4e827534f86122984448561d58
Jared Hanson <jaredhanson@gmail.com>
2021-11-15 17:17:59 -0800

    Add test.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -565,6 +565,86 @@ describe('Strategy', function() {
       .authenticate();
   }); // should redirect with claims parameter
   
+  it('should authenticate request', function(done) {
+    var strategy = new Strategy({
+      issuer: 'https://server.example.com',
+      authorizationURL: 'https://server.example.com/authorize',
+      tokenURL: 'https://server.example.com/token',
+      userInfoURL: 'https://server.example.com/userinfo',
+      clientID: 's6BhdRkqt3',
+      clientSecret: 'some_secret12345',
+      callbackURL: 'https://client.example.org/cb'
+    },
+    function(iss, sub, profile, accessToken, refreshToken, cb) {
+      return cb(null, { id: '248289761001' });
+    });
+    
+    sinon.stub(strategy._oauth2, 'getOAuthAccessToken').yieldsAsync(null, 'SlAV32hkKG', '8xLOxBtZp8', {
+      token_type: 'Bearer',
+      expires_in: 3600,
+      id_token: jws.sign({
+        header: { alg: 'HS256' },
+        payload: {
+          iss: 'https://server.example.com',
+          sub: '248289761001',
+          aud: 's6BhdRkqt3',
+          exp: Math.floor((Date.now() + 1000000) / 1000),
+          iat: Math.floor(Date.now() / 1000)
+        },
+        secret: 'keyboard cat',
+      })
+    });
+    
+    sinon.stub(strategy._oauth2, '_request').yieldsAsync(null, JSON.stringify({
+      sub: '248289761001',
+      name: 'Jane Doe',
+      given_name: 'Jane',
+      family_name: 'Doe',
+      preferred_username: 'j.doe',
+      email: 'janedoe@example.com',
+      picture: 'http://example.com/janedoe/me.jpg'
+    }));
+    
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.query = {
+          code: 'SplxlOBeZQQYbYS6WxSbIA',
+          state: 'af0ifjsldkj'
+        };
+        req.session = {};
+        req.session['openidconnect:server.example.com'] = {
+          state: {
+            handle: 'af0ifjsldkj'
+          }
+        };
+      })
+      .success(function(user, info) {
+        expect(user).to.deep.equal({ id: '248289761001' });
+        expect(info).to.deep.equal({});
+        
+        expect(strategy._oauth2.getOAuthAccessToken.calledOnce).to.be.true;
+        expect(strategy._oauth2.getOAuthAccessToken.getCall(0).args[0]).to.equal('SplxlOBeZQQYbYS6WxSbIA');
+        expect(strategy._oauth2.getOAuthAccessToken.getCall(0).args[1]).to.deep.equal({
+          grant_type: 'authorization_code',
+          redirect_uri: 'https://client.example.org/cb'
+        });
+        
+        expect(strategy._oauth2._request.calledOnce).to.be.true;
+        expect(strategy._oauth2._request.getCall(0).args[0]).to.equal('GET');
+        expect(strategy._oauth2._request.getCall(0).args[1]).to.equal('https://server.example.com/userinfo?schema=openid');
+        expect(strategy._oauth2._request.getCall(0).args[2]).to.deep.equal({
+          'Authorization': 'Bearer SlAV32hkKG',
+          'Accept': 'application/json'
+        });
+        expect(strategy._oauth2._request.getCall(0).args[3]).to.be.null;
+        expect(strategy._oauth2._request.getCall(0).args[4]).to.be.null;
+        
+        done();
+      })
+      .error(done)
+      .authenticate();
+  }); // should authenticate request
+  
   it('should authenticate request where audience claim contains the client ID value and authorized party claim matches client ID', function(done) {
     var strategy = new Strategy({
       issuer: 'https://server.example.com',
