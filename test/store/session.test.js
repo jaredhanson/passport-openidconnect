51ebf11bca1fa8ae6b96c2ab06a5d9b4c9f001a8
Jared Hanson <jaredhanson@gmail.com>
2021-11-17 11:53:32 -0800

    Clean up.

--- test/store/session.test.js
+++ test/store/session.test.js
@@ -3,6 +3,7 @@ var sinon = require('sinon');
 var Strategy = require('../../lib/strategy');
 var uri = require('url');
 var jwt = require('jsonwebtoken');
+var jws = require('jws');
 
 
 describe('SessionStore', function() {
@@ -24,7 +25,7 @@ describe('SessionStore', function() {
       tokenURL: 'https://server.example.com/token',
       clientID: 's6BhdRkqt3',
       clientSecret: 'some_secret12345',
-      callbackURL: 'https://client.example.org/cb'
+      callbackURL: 'https://client.example.com/cb'
     },
     function(iss, profile, done) {
       throw new Error('verify function should not be called');
@@ -54,7 +55,7 @@ describe('SessionStore', function() {
             authorizationURL: 'https://server.example.com/authorize',
             tokenURL: 'https://server.example.com/token',
             clientID: 's6BhdRkqt3',
-            callbackURL: 'https://client.example.org/cb'
+            callbackURL: 'https://client.example.com/cb'
           });
           
           done();
@@ -68,7 +69,7 @@ describe('SessionStore', function() {
         .request(function(req) {
           req.session = {};
           req.session['openidconnect:server.example.com'] = {
-            returnTo: 'https://client.example.org/welcome'
+            returnTo: 'https://client.example.com/app'
           };
         })
         .redirect(function(url) {
@@ -77,7 +78,7 @@ describe('SessionStore', function() {
           
           expect(state).to.have.length(24);
           expect(this.session['openidconnect:server.example.com']).to.deep.equal({
-            returnTo: 'https://client.example.org/welcome',
+            returnTo: 'https://client.example.com/app',
             state: {
               handle: state
             }
@@ -88,16 +89,6 @@ describe('SessionStore', function() {
         .authenticate();
     }); // should store strategy-specific state in session alongside state set manually by app
     
-    it('should error when app does not have session support', function(done) {
-      chai.passport.use(strategy)
-        .error(function(err) {
-          expect(err).to.be.an.instanceof(Error)
-          expect(err.message).to.equal('OpenID Connect authentication requires session support when using state. Did you forget to use express-session middleware?');
-          done();
-        })
-        .authenticate();
-    }); // should error when app does not have session support
-    
     it('should store strategy-specific state in session under session key', function(done) {
       var strategy = new Strategy({
         issuer: 'https://server.example.com',
@@ -105,7 +96,7 @@ describe('SessionStore', function() {
         tokenURL: 'https://server.example.com/token',
         clientID: 's6BhdRkqt3',
         clientSecret: 'some_secret12345',
-        callbackURL: 'https://client.example.org/cb',
+        callbackURL: 'https://client.example.com/cb',
         sessionKey: 'openidconnect:example'
       },
       function(iss, profile, done) {
@@ -132,6 +123,16 @@ describe('SessionStore', function() {
         .authenticate();
     }); // should store strategy-specific state in session under session key
     
+    it('should error when app does not have session support', function(done) {
+      chai.passport.use(strategy)
+        .error(function(err) {
+          expect(err).to.be.an.instanceof(Error)
+          expect(err.message).to.equal('OpenID Connect requires session support. Did you forget to use `express-session` middleware?');
+          done();
+        })
+        .authenticate();
+    }); // should error when app does not have session support
+    
   }); // #store
     
   describe('#verify', function() {
@@ -142,16 +143,26 @@ describe('SessionStore', function() {
       userInfoURL: 'https://server.example.com/userinfo',
       clientID: 's6BhdRkqt3',
       clientSecret: 'some_secret12345',
-      callbackURL: 'https://client.example.org/cb'
+      callbackURL: 'https://client.example.com/cb'
     },
     function(iss, profile, done) {
-      return done(null, { id: '248289761001' }, { message: 'Hello' });
+      return done(null, { id: '248289761001' });
     });
     
     sinon.stub(strategy._oauth2, 'getOAuthAccessToken').yieldsAsync(null, 'SlAV32hkKG', '8xLOxBtZp8', {
       token_type: 'Bearer',
       expires_in: 3600,
-      id_token: buildIdToken()
+      id_token: jws.sign({
+        header: { alg: 'HS256' },
+        payload: {
+          iss: 'https://server.example.com',
+          sub: '248289761001',
+          aud: 's6BhdRkqt3',
+          exp: Math.floor((Date.now() + 1000000) / 1000),
+          iat: Math.floor(Date.now() / 1000)
+        },
+        secret: 'keyboard cat',
+      })
     });
     
     sinon.stub(strategy._oauth2, 'get').yieldsAsync(null, JSON.stringify({
@@ -195,7 +206,7 @@ describe('SessionStore', function() {
           };
           req.session = {};
           req.session['openidconnect:server.example.com'] = {
-            returnTo: 'https://client.example.org/welcome',
+            returnTo: 'https://client.example.com/app',
             state: {
               handle: 'af0ifjsldkj'
             }
@@ -203,93 +214,13 @@ describe('SessionStore', function() {
         })
         .success(function(user, info) {
           expect(this.session['openidconnect:server.example.com']).to.deep.equal({
-            returnTo: 'https://client.example.org/welcome',
+            returnTo: 'https://client.example.com/app',
           });
           done();
         })
         .error(done)
         .authenticate();
     }); // should not remove state set manually by application from session when successfully verified
-    
-    it('should fail if state is not bound to session', function(done) {
-      chai.passport.use(strategy)
-        .request(function(req) {
-          req.query = {
-            code: 'SplxlOBeZQQYbYS6WxSbIA',
-            state: 'XXXXXXXX'
-          };
-          req.session = {};
-          req.session['openidconnect:server.example.com'] = {};
-          req.session['openidconnect:server.example.com'] = {
-            state: {
-              handle: 'af0ifjsldkj'
-            }
-          };
-        })
-        .fail(function(info, status) {
-          expect(info).to.deep.equal({ message: 'Invalid authorization request state.' });
-          expect(status).to.equal(403);
-          // FIXME: Should state be preserved in this case?
-          expect(this.session['openidconnect:server.example.com']).to.be.undefined;
-          done();
-        })
-        .error(done)
-        .authenticate();
-    }); // should fail if state is not bound to session
-    
-    it('should fail if provider-specific state is not found in session', function(done) {
-      chai.passport.use(strategy)
-        .request(function(req) {
-          req.query = {
-            code: 'SplxlOBeZQQYbYS6WxSbIA',
-            state: 'af0ifjsldkj'
-          };
-          req.session = {};
-        })
-        .fail(function(info, status) {
-          expect(info).to.deep.equal({ message: 'Unable to verify authorization request state.' });
-          expect(status).to.equal(403);
-          done();
-        })
-        .error(done)
-        .authenticate();
-    }); // should fail if provider-specific state is not found in session
-    
-    it('should fail if provider-specific state is missing state handle', function(done) {
-      chai.passport.use(strategy)
-        .request(function(req) {
-          req.query = {
-            code: 'SplxlOBeZQQYbYS6WxSbIA',
-            state: 'af0ifjsldkj'
-          };
-          req.session = {};
-          req.session['openidconnect:server.example.com'] = {};
-        })
-        .fail(function(info, status) {
-          expect(info).to.deep.equal({ message: 'Unable to verify authorization request state.' });
-          expect(status).to.equal(403);
-          expect(this.session['openidconnect:server.example.com']).to.deep.equal({});
-          done();
-        })
-        .error(done)
-        .authenticate();
-    }); // should fail if provider-specific state is missing state handle
-    
-    it('should error when app does not have session support', function(done) {
-      chai.passport.use(strategy)
-        .request(function(req) {
-          req.query = {
-            code: 'SplxlOBeZQQYbYS6WxSbIA',
-            state: 'af0ifjsldkj'
-          };
-        })
-        .error(function(err) {
-          expect(err).to.be.an.instanceof(Error)
-          expect(err.message).to.equal('OpenID Connect authentication requires session support when using state. Did you forget to use express-session middleware?');
-          done();
-        })
-        .authenticate();
-    }); // should error when app does not have session support
     
     it('should remove state from session under session key when successfully verified', function(done) {
       var strategy = new Strategy({
@@ -342,6 +273,86 @@ describe('SessionStore', function() {
         .error(done)
         .authenticate();
     }); // should remove state from session under session key when successfully verified
+    
+    it('should fail if state is not bound to session', function(done) {
+      chai.passport.use(strategy)
+        .request(function(req) {
+          req.query = {
+            code: 'SplxlOBeZQQYbYS6WxSbIA',
+            state: 'XXXXXXXX'
+          };
+          req.session = {};
+          req.session['openidconnect:server.example.com'] = {};
+          req.session['openidconnect:server.example.com'] = {
+            state: {
+              handle: 'af0ifjsldkj'
+            }
+          };
+        })
+        .fail(function(info, status) {
+          expect(info).to.deep.equal({ message: 'Invalid authorization request state.' });
+          expect(status).to.equal(403);
+          // FIXME: Should state be preserved in this case?
+          expect(this.session['openidconnect:server.example.com']).to.be.undefined;
+          done();
+        })
+        .error(done)
+        .authenticate();
+    }); // should fail if state is not bound to session
+    
+    it('should fail if provider-specific state is not available in session', function(done) {
+      chai.passport.use(strategy)
+        .request(function(req) {
+          req.query = {
+            code: 'SplxlOBeZQQYbYS6WxSbIA',
+            state: 'af0ifjsldkj'
+          };
+          req.session = {};
+        })
+        .fail(function(info, status) {
+          expect(info).to.deep.equal({ message: 'Unable to verify authorization request state.' });
+          expect(status).to.equal(403);
+          done();
+        })
+        .error(done)
+        .authenticate();
+    }); // should fail if provider-specific state is not available in session
+    
+    it('should fail if provider-specific state is missing state', function(done) {
+      chai.passport.use(strategy)
+        .request(function(req) {
+          req.query = {
+            code: 'SplxlOBeZQQYbYS6WxSbIA',
+            state: 'af0ifjsldkj'
+          };
+          req.session = {};
+          req.session['openidconnect:server.example.com'] = {};
+        })
+        .fail(function(info, status) {
+          expect(info).to.deep.equal({ message: 'Unable to verify authorization request state.' });
+          expect(status).to.equal(403);
+          expect(this.session['openidconnect:server.example.com']).to.deep.equal({});
+          done();
+        })
+        .error(done)
+        .authenticate();
+    }); // should fail if provider-specific state is missing state
+    
+    it('should error when app does not have session support', function(done) {
+      chai.passport.use(strategy)
+        .request(function(req) {
+          req.query = {
+            code: 'SplxlOBeZQQYbYS6WxSbIA',
+            state: 'af0ifjsldkj'
+          };
+        })
+        .error(function(err) {
+          expect(err).to.be.an.instanceof(Error)
+          expect(err.message).to.equal('OpenID Connect requires session support. Did you forget to use `express-session` middleware?');
+          done();
+        })
+        .authenticate();
+    }); // should error when app does not have session support
     
   }); // #verify
   
